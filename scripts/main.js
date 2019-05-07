79e8309523a577b8e4b7c2fcab6cab274fa02408
strvworks <strv13570@gmail.com>
2019-05-07 19:26:44 +0900

    ノードが削除できるようになった(バグあり)

--- scripts/main.js
+++ scripts/main.js
@@ -38,6 +38,10 @@ class NodesController {
         this.registerNode(new BranchNode(x, y, generateUuid(), this.stage));
     }
 
+    addEventNode(x, y) {
+        this.registerNode(new EventNode(x, y, generateUuid(), this.stage));
+    }
+
     registerNode(node) {
         this.
         nodes.push(node);
@@ -52,6 +56,7 @@ class NodesController {
 
 class MessageNode {
     constructor(x, y, uuid, stage) {
+        // ノード基本情報
         this.type = "message";
         this.sizeX = 150;
         this.sizeY = 50;
@@ -62,6 +67,10 @@ class MessageNode {
 
         this.stage = stage;
 
+        // ノード固有情報
+        this.text = "";
+
+        // 描画情報
         this.line = new createjs.Shape();
         this.stage.addChild(this.line);
         this.stage.setChildIndex(this.line, 0);
@@ -83,6 +92,7 @@ class MessageNode {
 
         this.part_main.addEventListener("mousedown", (evt) => {
             app.selected_node_uuid = this.UUID;
+            app.message_node_text = this.text;
         });
         this.part_main.addEventListener("pressmove", (evt) => {
             this.x = snap(this.stage.mouseX) + this.stage.regX;
@@ -135,11 +145,126 @@ class MessageNode {
         }
     }
 
+    destroy() {
+        this.stage.removeChild(this.part_main);
+        this.stage.removeChild(this.input);
+        this.stage.removeChild(this.output);
+        this.stage.removeChild(this.line);
+    }
 }
 
 class BranchNode {
     constructor(x, y, uuid, stage) {
+        // ノード基本情報
         this.type = "branch";
+        this.sizeX = 150;
+        this.sizeY = 50;
+        this.x = x;
+        this.y = y;
+        this.UUID = uuid;
+        this.nextNodesUUID = ["", "", "", ""];
+
+        this.stage = stage;
+
+        // ノード固有情報
+        this.choices = ["", "", "", ""];
+
+        // 描画情報
+        this.part_main = new createjs.Shape();
+        this.part_main.graphics.beginFill("#cca1ea");
+        this.part_main.graphics.drawRoundRect(0, 0, this.sizeX, this.sizeY, 10);
+        this.stage.addChild(this.part_main);
+
+        this.input = new createjs.Shape();
+        this.input.graphics.beginFill("#747474");
+        this.input.graphics.drawRoundRect(0, 0, this.sizeX*0.8, this.sizeY*0.25, 3);
+        this.stage.addChild(this.input);
+
+        this.outputs = [];
+        this.offsetsX = [-0.2, 0.01, 0.23, 0.45];
+        this.lines = [];
+        for (let i = 0; i < 4; i++) {
+            this.outputs.push(new createjs.Shape());
+            this.outputs[i].graphics.beginFill("#575757");
+            this.outputs[i].graphics.drawRoundRect(0, 0, this.sizeX*0.2, this.sizeY*0.25, 3);
+            this.stage.addChild(this.outputs[i]);
+
+            this.lines.push(new createjs.Shape());
+            this.stage.addChild(this.lines[i]);
+            this.stage.setChildIndex(this.lines[i], 0);
+
+            this.outputs[i].addEventListener("mousedown", (evt) => {
+                this.nextNodesUUID[i] = null;
+            });
+            this.outputs[i].addEventListener("pressmove", (evt) => {
+                this.lines[i].graphics.clear();
+                app.drawLine(this.x-(this.sizeX*this.offsetsX[i])+(this.sizeX*0.1), this.y, this.stage.mouseX, this.stage.mouseY, this.lines[i]);
+            });
+            this.outputs[i].addEventListener("pressup", (evt) => {
+                this.lines[i].graphics.clear();
+                let under_object = stage.getObjectsUnderPoint(stage.mouseX, stage.mouseY);
+                if (under_object.length >= 1) {
+                    let uuid = app.getUuidByObject(under_object[0]);
+                    if (uuid !== null && uuid !== this.UUID) {
+                        this.nextNodesUUID[i] = uuid;
+                    }
+                }
+            });
+        }
+
+        this.part_main.addEventListener("mousedown", (evt) => {
+            app.selected_node_uuid = this.UUID;
+            app.branch_node_choices = this.choices;
+        });
+        this.part_main.addEventListener("pressmove", (evt) => {
+            this.x = snap(this.stage.mouseX) + this.stage.regX;
+            this.y = snap(this.stage.mouseY) + this.stage.regY;
+        });
+        this.part_main.addEventListener("pressup", (evt) => {
+            console.log(this.UUID);
+            console.log(this.nextNodesUUID);
+        });
+
+        this.input.addEventListener("pressmove", (evt) => {
+            console.log("input");
+        });
+
+        createjs.Ticker.addEventListener("tick", () => {this.update()});
+    }
+
+    update() {
+        this.part_main.x = (this.x - this.sizeX / 2);
+        this.part_main.y = (this.y - this.sizeY / 2);
+
+        this.input.x = (this.x - this.sizeX*0.8 / 2);
+        this.input.y = (this.y - this.sizeY*0.71);
+
+        for (let i = 0; i < 4; i++) {
+            this.outputs[i].x = (this.x - this.sizeX*this.offsetsX[i]);
+            this.outputs[i].y = (this.y + this.sizeY*0.50);
+            let nextNode = app.getObjectByUuid(this.nextNodesUUID[i]);
+            if (nextNode !== null) {
+                this.lines[i].graphics.clear();
+                app.drawLine(this.x-(this.sizeX*this.offsetsX[i])+(this.sizeX*0.1), this.y, nextNode.x, nextNode.y-20, this.lines[i]);
+            }
+        }
+
+    }
+
+    destroy() {
+        this.stage.removeChild(this.part_main);
+        this.stage.removeChild(this.input);
+        for (let i = 0; i < 4; i++) {
+            this.stage.removeChild(this.outputs[i]);
+            this.stage.removeChild(this.lines[i]);
+        }
+    }
+}
+
+class EventNode {
+    // ノード基本情報
+    constructor(x, y, uuid, stage) {
+        this.type = "event";
         this.sizeX = 150;
         this.sizeY = 50;
         this.x = x;
@@ -149,12 +274,16 @@ class BranchNode {
 
         this.stage = stage;
 
+        // ノード固有情報
+        this.eventId = 0;
+
+        // 描画情報
         this.line = new createjs.Shape();
         this.stage.addChild(this.line);
         this.stage.setChildIndex(this.line, 0);
 
         this.part_main = new createjs.Shape();
-        this.part_main.graphics.beginFill("#cca1ea");
+        this.part_main.graphics.beginFill("#ea6059");
         this.part_main.graphics.drawRoundRect(0, 0, this.sizeX, this.sizeY, 10);
         this.stage.addChild(this.part_main);
 
@@ -222,9 +351,16 @@ class BranchNode {
         }
     }
 
+    destroy() {
+        this.stage.removeChild(this.part_main);
+        this.stage.removeChild(this.input);
+        this.stage.removeChild(this.output);
+        this.stage.removeChild(this.line);
+    }
 }
 
 class StartNode {
+    // ノード基本情報
     constructor(x, y, uuid, stage) {
         this.type = "start";
         this.sizeX = 50;
@@ -236,6 +372,7 @@ class StartNode {
 
         this.stage = stage;
 
+        // 描画情報
         this.line = new createjs.Shape();
         this.stage.addChild(this.line);
         this.stage.setChildIndex(this.line, 0);
@@ -297,9 +434,16 @@ class StartNode {
             app.drawLine(this.x, this.y, nextNode.x, nextNode.y-20, this.line);
         }
     }
+
+    destroy() {
+        this.stage.removeChild(this.part_main);
+        this.stage.removeChild(this.output);
+        this.stage.removeChild(this.line);
+    }
 }
 
 class EndNode{
+    // ノード基本情報
     constructor(x, y, uuid, stage) {
         this.type = "end";
         this.sizeX = 50;
@@ -310,6 +454,7 @@ class EndNode{
 
         this.stage = stage;
 
+        // 描画情報
         this.part_main = new createjs.Shape();
         this.part_main.graphics.beginFill("#1f1e21");
         this.part_main.graphics.drawRoundRect(0, 0, this.sizeX, this.sizeY, 10);
@@ -346,6 +491,10 @@ class EndNode{
         this.input.y = (this.y - this.sizeY*0.95);
     }
 
+    destroy() {
+        this.stage.removeChild(this.part_main);
+        this.stage.removeChild(this.input);
+    }
 }
 
 let app = new Vue({
@@ -355,6 +504,9 @@ let app = new Vue({
         stage: null,
         selected_node_uuid: "none",
         node_controller: null,
+
+        message_node_text: "",
+        branch_node_choices: ["", "", "", ""],
     },
     created: () => {
         createjs.Ticker.framerate = 60;
@@ -371,6 +523,21 @@ let app = new Vue({
         },
         addBranchNode: () => {
             this.node_controller.addBranchNode(this.stage.mouseX-500, this.stage.mouseY+50);
+        },
+        addEventNode: () => {
+            this.node_controller.addEventNode(this.stage.mouseX-500, this.stage.mouseY+50);
+        },
+        changeMessage: function() {
+            let obj = this.getObjectByUuid(this.selected_node_uuid);
+            if (obj !== null) {
+                obj.text = this.message_node_text;
+            }
+        },
+        changeChoices: function() {
+            let obj = this.getObjectByUuid(this.selected_node_uuid);
+            if (obj !== null) {
+                obj.choices = this.branch_node_choices;
+            }
         },
         getUuidByObject: (obj) => {
             let uuid = null;
@@ -406,10 +573,33 @@ let app = new Vue({
                 line.graphics.lineTo(x2, y2 - 50);
                 line.graphics.lineTo(x2, y2);
             }
+        },
+        deleteNodeByUuid: (uuid) => {
+            let obj = null;
+            this.nodes.forEach((node) => {
+                if (node.UUID === uuid) {
+                    obj = node;
+                }
+            });
+            console.log(obj);
+            if (obj !== null) {
+                obj.destroy();
+            }
+
+            console.log(this.nodes);
+            this.nodes = this.nodes.filter((n) => {
+                if (n.UUID !== uuid){
+                    return n;
+                }
+            });
+            console.log(this.nodes);
+        },
+        deleteNode: function() {
+            this.deleteNodeByUuid(this.selected_node_uuid);
         }
     },
     computed: {
-        selected_node_name: function () {
+        selected_node_type: function () {
             let obj = this.getObjectByUuid(this.selected_node_uuid);
             let name = "none";
             if (obj !== null) {
@@ -437,4 +627,3 @@ function generateUuid() {
 function snap(num, snap=15) {
     return Math.round(num/snap)*snap;
 }
-
